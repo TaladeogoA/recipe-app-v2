e78f0c758454ba257956ba030affaf084a4e9f41
TaladeogoA <ataladeogo@gmail.com>
2024-01-01 04:44:51 +0100

    Update .env and next.config.js, and upgrade dependencies

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  images: {
+    domains: ["www.themealdb.com"],
+  },
 };
 
 module.exports = nextConfig;
