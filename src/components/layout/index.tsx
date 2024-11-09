75942f0cdd9d8215510fbdba81bc47ea0a53cfa1
TaladeogoA <ataladeogo@gmail.com>
2024-11-09 13:11:16 +0100

    Update styles and layout components; add featured recipe card and improve navbar

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from "next";
 import { ReactNode, useState } from "react";
-import { Raleway } from "next/font/google";
+import { Raleway, Inter } from "next/font/google";
 import Head from "next/head";
 import Link from "next/link";
 import layoutStyles from "./layout.module.scss";
@@ -11,6 +11,7 @@ import CookieIcon from "@/assets/icons/cookie.svg";
 import Logo from "@/assets/images/logo.svg";
 
 const raleway = Raleway({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 type Children = {
   children: ReactNode;
@@ -30,26 +31,8 @@ const GeneralLayout: NextPage<Children> = ({ children }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={`${layoutStyles.container} ${raleway.className}`}>
-        <div
-          className={layoutStyles.cookieButton}
-          onClick={() => setShowModal(true)}
-        >
-          <Image src={CookieIcon} width={35} height={35} alt="accept cookies" />
-        </div>
-        {showModal && (
-          <div className={layoutStyles.modal}>
-            <div className={layoutStyles.modalContent}>
-              <h2>No Cookies Here! 🍪</h2>
-              <p>
-                Just me having a bit of fun! I drew that cute cookie SVG and
-                thought it would be fun to add it to the site.
-              </p>
-              <button onClick={() => setShowModal(false)}>Ciao!</button>
-            </div>
-          </div>
-        )}
-        <div className={layoutStyles.sidebar}>
+      <div className={`${layoutStyles.container} ${inter.className}`}>
+        {/* <div className={layoutStyles.sidebar}>
           <div className={layoutStyles.logo}>
             <Image src={Logo} alt="app logo" />
           </div>
@@ -70,6 +53,31 @@ const GeneralLayout: NextPage<Children> = ({ children }) => {
             <FaLinkedinIn />
             <FaInstagram />
           </div>
+        </div> */}
+        <div className={layoutStyles.navbar}>
+          <div className={layoutStyles.navContainer}>
+            <div className={layoutStyles.logo}>
+              <Image src={Logo} alt="app logo" />
+              <p>forkcasted</p>
+            </div>
+
+            <nav className={layoutStyles.navItems}>
+              <ul>
+                <NavLink href="/" text="home" activeRoute={activeRoute} />
+                <NavLink
+                  href="/recipes"
+                  text="recipes"
+                  activeRoute={activeRoute}
+                />
+                <NavLink href="/about" text="about" activeRoute={activeRoute} />
+              </ul>
+            </nav>
+          </div>
+
+          <div className={layoutStyles.searchContainer}>
+            <input type="text" placeholder="Search for recipes" />
+            <button>Search</button>
+          </div>
         </div>
         <main className={layoutStyles.main}>{children}</main>
       </div>
@@ -89,8 +97,6 @@ const NavLink = ({
   activeRoute: string;
 }) => (
   <Link href={href}>
-    <li className={activeRoute === href ? layoutStyles.active : undefined}>
-      {text}
-    </li>
+    <li>{text}</li>
   </Link>
 );
--- src/pages/recipes/index.tsx
+++ src/pages/recipes/index.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+import FeaturedRecipeCard from "@/components/featured-recipe-card";
+import RecipesStyles from "./recipes.module.scss";
+import { useQuery } from "@tanstack/react-query";
 
 const Recipes = () => {
-  return <div></div>;
+  const { data, isLoading } = useQuery({
+    queryKey: ["featured"],
+    queryFn: async () => {
+      const res = await fetch(`
+      ${process.env.NEXT_PUBLIC_API_URL}/lookup.php?i=52812`);
+      return res.json();
+    },
+  });
+  console.log("data", data);
+  const recipe = data?.meals[0];
+
+  return (
+    <div className={RecipesStyles.container}>
+      {/* <div className={RecipesStyles.hero}>
+        <h1>All Recipes</h1>
+        <p>
+          Explore a wide range of recipes, from classic comfort foods to exotic
+          cuisines.
+        </p>
+      </div> */}
+
+      <div className={RecipesStyles.featured}>
+        <div className={RecipesStyles.hero}>
+          <h2>Recipe of the Day</h2>
+          <p>
+            Explore a wide range of recipes, from classic comfort foods to
+            exotic cuisines.
+          </p>
+        </div>
+        <FeaturedRecipeCard recipe={recipe} isLoading={isLoading} />
+      </div>
+
+      <div>
+        <h3>Filter by Category</h3>
+        <div>
+          <p>All</p>
+          <p>Breakfast</p>
+          <p>Lunch</p>
+          <p>Dinner</p>
+          <p>Dessert</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Recipes;
